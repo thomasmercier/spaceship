b9e80bf3f64e6ce6b010e6c1d399b7cc8aa437ae
Thomas Mercier <thomas.mercier@tagheuer.com>
2017-12-04 15:02:29 +0100

    operational version

--- agent.py
+++ agent.py
@@ -1,37 +1,47 @@
 import numpy as np
 import shapely.geometry
 import shapely.prepared
+from scipy.integrate import odeint
+import matplotlib.pyplot as plt
 
-class OccupancyMap:
+class Trajectory:
 
-    def __init__(self, nx, xmin, xmax, ny, ymin, ymax):
+    def __init__(self, xmin, xmax, ymin, ymax, dpi, X0, thickness):
 
-        self.nx = nx
+        self.nx = int(dpi*(xmax-xmin))
         self.xmin = xmin
         self.xmax = xmax
-        self.ny = ny
+        self.ny = int(dpi*(ymax-ymin))
         self.ymin = ymin
         self.ymax = ymax
+        self.thickness = thickness
 
+        self.centroid_geom = shapely.geometry.Point(X0[0:2])
+        self.occupancy_geom = self.centroid_geom.buffer(self.thickness)
         self.occupancy_px = np.zeros((self.nx, self.ny), dtype=np.bool)
-        self.occupancy_geom = None
 
-        self.px_points = [None]*nx*ny
-        dx = (xmax-xmin) / nx
-        dy = (ymax-ymin) / ny
-        for i in range(nx):
-            for j in range(ny):
-                px_points[i+nx*j] = shapely.geometry.Point((xmin+i*dx,ymin+j*dy))
+        self.px_points = [None]*self.nx*self.ny
+        dx = (xmax-xmin) / self.nx
+        dy = (ymax-ymin) / self.ny
+        for i in range(self.nx):
+            for j in range(self.ny):
+                self.px_points[i+self.nx*j] = shapely.geometry.Point((xmin+i*dx,ymin+j*dy))
 
-    def add_polyline(points, thickness):
-        
-        ls = shapely.geometry.LineStrings(points).buffer(thickness)
-        self.occupancy_geom = self.occupancy_geom.union(ls)
-        prepared_ls = shapely.prepared.prep(ls)
-        interior = map(prepared_polygon.contains, self.py_points)
+    def add_polyline(self, points):
+
+        ls = shapely.geometry.LineString(points)
+        ls_thick = ls.buffer(self.thickness)
+        self.centroid_geom = self.centroid_geom.union(ls)
+        self.occupancy_geom = self.occupancy_geom.union(ls_thick)
+        prepared_ls_thick = shapely.prepared.prep(ls_thick)
+        interior = list(map(prepared_ls_thick.contains, self.px_points))
         ls_occupancy_px = np.array(interior, dtype=np.bool).reshape((self.nx,self.ny))
         self.occupancy_px = np.logical_or(self.occupancy_px, ls_occupancy_px)
 
+    def plot_px(self):
+
+        plt.imshow(self.occupancy_px)
+        plt.show()
 
 class Agent:
 
@@ -40,35 +50,26 @@ class Agent:
         self.X0 = X0
         self.X = Xinit
         self.t = 0
-        self.world_size = 100
-        self.world_size_px = 1000
-        self.occupancy = OccupancyMap(self.world_size_px, 0, self.world_size = 100, \
-                                      self.world_size_px, 0, self.world_size = 100, )
+        self.trajectory = Trajectory(0, 100, 0, 100, 10, X0[0:2], 5)
+        self.k = 1
 
     def dXdt(self, X, u):
 
         dXdt0 = X[2:4]
-        dXdt1 = self.k*(X[0:2]-self.X0) + u
+        dXdt1 = - self.k*(X[0:2]-self.X0) + u
         return np.append(dXdt0, dXdt1)
-
-    def draw_thick_line(X0, X1, thickness):
-
-        if np.array_equal(X0, X1):
-            return np.zeros((self.world_size,self.world_size), dtype=np.bool)
-        else:
-            T = (X1-X0) / np.linalg.norm(X1-X0)
-            N = np.array( [T[0], -T[1]] )
-            X00 = X0 + thickness/2. * (N-T)
-            X11 = X1 + thickness/2. * (T-N)
-            return skimage.draw.rectangle(start=X00, end=X11, shape=[self.world_size, self.world_size])
-
 
     def move(self, u, dt):
 
-        nPoints = 10
+        nPoints = 100
         t = np.linspace(self.t, self.t+dt, nPoints, endpoint=True);
         def dXdt(X, t):
             return self.dXdt(X,u)
-        trajectory = scipy.integrate.odeint(dXdt, self.X, t)
+        new_trajectory = odeint(dXdt, self.X, t)
         self.t += dt
-        self.X = trajectory[nPoints-1]
+        self.X = new_trajectory[nPoints-1,:]
+        self.trajectory.add_polyline(new_trajectory[:,0:2])
+
+agent = Agent((50,50), (50, 70, 15, 0))
+agent.move((1, 0), 10)
+agent.trajectory.plot_px()
